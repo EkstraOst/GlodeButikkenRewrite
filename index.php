6fdda63bf5667b5e0d2948841ad48f126f8b09cc
Bjørnar <gouda@ekstraost.xyz>
2024-03-02 22:07:44 +0100

    Fix på oversette ting. Pluss at tåpelig extension resetter prosjektet hvis jeg ikke commiter. :S

--- index.php
+++ index.php
@@ -7,70 +7,39 @@
     <link rel="stylesheet" href="Assets/css/styleLp.css"/>
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="Assets/css/styleMeny.css"/>
-    <?php 
-      if (isset($_GET['page']) && $_GET['page'] == 3) {
-        echo '<link rel="stylesheet" href="Assets/css/textOmOss.css"/>';
-        echo '<link rel="stylesheet" href="Assets/css/mediaQueryOmOss.css"/>';
-      }
-    ?>
-    <script src="Assets/js/script.js" async defer></script>
-    
-    <!--  <link rel="stylesheet" href="products.css"/>  -->
-    
-    <!-- <link rel="stylesheet" href="/GlodeButikken/style.css">
-    <link rel="stylesheet" href="/GlodeButikken/imageanimation.css">
-    <link rel="stylesheet" href="/GlodeButikken/slide.css"> -->
+    <link rel="stylesheet" href="Assets/css/textOmOss.css"/>
+    <link rel="stylesheet" href="Assets/css/mediaQueryOmOss.css"/>
 
-    <!-- Tab title-->
     <title>Gløde Data</title>
-
   </head>
 
-<body>
 
-<?php 
-  error_reporting(E_ALL);
-  ini_set('display_errors', '1');
-  session_start();
+  <?php 
+    //DEBUG
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
 
-  $_SESSION['page'] =  isset($_GET['page']) ? $_GET['page'] : "0";
-  $_SESSION['type'] =  isset($_GET['type']) ? $_GET['type'] : "0";
-  $_SESSION['param'] = isset($_GET['param']) ? $_GET['param'] : "^.*%";
+    //Start session og set variabler til valg hvis noe er i GET eller default hvis ikke.
+    session_start();
+    $_SESSION['page'] =  isset($_GET['page']) ? $_GET['page'] : "0";
+    $_SESSION['type'] =  isset($_GET['type']) ? $_GET['type'] : "0";
+    $_SESSION['param'] = isset($_GET['param']) ? $_GET['param'] : "^.*%";
 
-  include("Assets/templates/header1.php"); 
+    //HEADER
+    include("Assets/templates/header1.php"); 
 
-
-  //DEBUG
-  //end DEBUG
-
-?>
-
-  <main>
-      <?php
-        if ($_SESSION['page'] == 2) {
-          include('Assets/page/sok.php');
-        } else if ($_SESSION['page'] == 3) {
-          include('Assets/page/om.php');
-        } else {
-          include('Assets/page/hjem.php');
-        }
-      ?>
-  </main>
-
-
-
-<footer>
-    <!--- Sender deg videre til siden som knappene sender deg til originalt. -->
-    <div class="footer-btns">
-      <a href="https://glode.no/" class="footer-btn"> GLØDE ARBEIDSINKLUDERING AS </a>
-      <a href="https://glode.no/wp-content/uploads/2023/12/Personvernerklaering.pdf" class="footer-btn"> Personvernserklæring </a>
-      <a href="https://glode.no/glode-data/" class="footer-btn"> C Gløde Data Butikken </a>
-    </div>
-    <!-- linkene/placeholder over må endres til relevante linker -mvh Polly   .-DONE -->
-</footer>
-  </div>
-  <!-- <script src="/GlodeButikken/imageanimation.js"></script> -->
-  <!--Hæh? Vi må finne ut hvor imageanimation skal og plassere js filen til den på rett plass, kommenterer den ut foreløpig -->
-  <!--Links to an external JavaScript file for dynamic behavior.-->
-</body>
+    //SIDEVISNING
+    echo "<body>";
+    if ($_SESSION['page'] == 2) {
+      include('Assets/page/sok.php');
+    } else if ($_SESSION['page'] == 3) {
+      include('Assets/page/om.php');
+    } else {
+      include('Assets/page/hjem.php');
+    }
+    echo "</body>";
+    
+    //FOOTER
+    include('Assets/templates/footer1.html');
+  ?>
 </html>
