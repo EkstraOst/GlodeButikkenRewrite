cebf3834620d334ec3c2a939e91fd17e75addeca
Bjørnar <gouda@ekstraost.xyz>
2024-03-11 22:46:58 +0100

    urk

--- Assets/js/leggivogn.js
+++ Assets/js/leggivogn.js
@@ -7,7 +7,6 @@ for (const btn of collection) {
 }
 
 function leggivogn(produktid) {
-    if (element == "") return;
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
